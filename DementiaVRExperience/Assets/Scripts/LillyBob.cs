b7d98513349b803982108d3003376a3017b6eab5
MattHough1999 <matthough.uk@gmail.com>
2020-03-19 09:36:47 +0000

    Tweaked Terrain and water
    Added an animation to the water and made flat spots on the terrain for the tent and camp fire
    
    Also added a light to the inside of the tent and on top of the table
    
    Additionally levelled the table using rock models

---
+++ DementiaVRExperience/Assets/Scripts/LillyBob.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LillyBob : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float RotationSpeed = 25;
+    [SerializeField] Vector3 RotationMax = new Vector3(10f, 0f, 10f);
+    [SerializeField] Vector3 RotationMin = new Vector3(-10f, 0f, -10f);
+    Vector3 current;
+    Vector3 BobMax;
+    Vector3 BobMin;
+
+    private void Start()
+    {
+        RotationMax += transform.rotation.eulerAngles;
+        RotationMin += transform.rotation.eulerAngles;
+        
+        current = this.transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+
+        this.transform.Rotate(new Vector3(0f, 0f, RotationSpeed) * Time.deltaTime);
+    }
+
+    
+}
---
+++ DementiaVRExperience/Assets/Scripts/WaterRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float RotationSpeed = 25;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        this.transform.Rotate(new Vector3(0f,0f,RotationSpeed) * Time.deltaTime);
+    }
+}
